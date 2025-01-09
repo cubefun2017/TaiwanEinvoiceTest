83b974eb55b0d2262fc7ed116abe4f878ea88787
夏之洋 <charlie@fmp.com.tw>
2025-01-09 11:38:10 +0800

    Add F0501 cancel created eInvoice

---
+++ F0501Test/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using MIG4.Storage;
+
+namespace F0501Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create F0501 Cancel eInvoice for storage
+            InvoiceF0501 F0501 = new InvoiceF0501();
+
+            // create blank XML file
+            //string fileName = "F0501test.xml";
+            //F0501.CreateXML(F0501, fileName);
+
+            // Create XML example file
+            var f0501 = F0501.CreateExample();
+            // override InvoiceNumber default value
+            //f0401.MainDetails.InvoiceNumber = "1234567890"; //invalid invoice number
+
+            // Valid format error
+            List<string> errors = F0501.Validation(f0501);
+            if (errors.Count == 0)
+            {
+                // Create example F0401.XML
+                string fileName = "F0501test.xml";
+                F0501.CreateXML(f0501, fileName);
+                Console.WriteLine($"{fileName} Created.");
+            }
+            else
+            {
+                // show all errors from Validation()
+                ShowErrors(errors);
+            }
+
+            Console.WriteLine("Press ANY key to exit.");
+            Console.ReadKey();
+        }
+        private static void ShowErrors(List<string> errors)
+        {
+            Console.WriteLine("Show Errors!");
+            foreach (string error in errors)
+            {
+                Console.WriteLine(error);
+            }
+        }
+    }
+}
